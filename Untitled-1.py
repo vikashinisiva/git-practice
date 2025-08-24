5d0a7711e388eceba36c97ef6bcb64ebc1ced58e
Vishal M <114340848+vishal-79@users.noreply.github.com>
2025-08-24 12:37:44 +0530

    i changed it to a odd or even code

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,16 @@
-print("helloworld")
+# Simple Odd or Even Checker
+
+def check_odd_even(number):
+    """Check if a number is odd or even"""
+    if number % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+# Get input from user
+try:
+    num = int(input("Enter a number: "))
+    result = check_odd_even(num)
+    print(f"{num} is {result}")
+except ValueError:
+    print("Please enter a valid integer!")
